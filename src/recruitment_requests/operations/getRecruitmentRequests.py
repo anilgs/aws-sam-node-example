68c9f50a522447ef2b54ea9a31715e2a64c5a944
Anil Saraswathy <anil.gs@inapp.com>
2022-04-21 15:17:37 +0530

    Linkage related changes

---
+++ src/recruitment_requests/operations/getRecruitmentRequests.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import boto3
+import os
+from botocore.exceptions import ClientError
+from aws_lambda_powertools import Logger, Metrics, Tracer
+from aws_lambda_powertools.metrics import MetricUnit
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+tablename = os.getenv("TABLE_NAME")
+
+tracer = Tracer(service="Get")
+logger = Logger(service="Get")
+metrics = Metrics(service="Get", namespace="GetService")
+
+
+def get_recruitment_requests(event):
+    log.debug("Received in get_recruitment_requests: {}".format(json.dumps(event)))
+    id = event['pathParameters']['id']
+    log.debug("id: {}".format(id))
+    try:
+
+        return respond(data=resp, status=200)
+    except ClientError as e:
+        print(e)
+        return respond(data="Operation failed", status=500)
+
+@metrics.log_metrics(capture_cold_start_metric=True)
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context
+def lambda_handler(event, context):
+
+    metrics.add_metric(name="GetSucceeded", value=1, unit=MetricUnit.Count)
+
+    log.debug("Received event in get_item: {}".format(json.dumps(event)))
+    return get_recruitment_requests(event)
+
+def respond(data="", status=200):
+    return {
+        "statusCode": status,
+        "body": json.dumps(data)
+    }
